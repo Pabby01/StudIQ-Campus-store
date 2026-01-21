274081508f424b969e7ba15d6ffae7624caaddaf
Pabby <adeolalasisi6@gmail.com>
2026-01-21 20:15:41 +0100

    updated

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect, useState } from "react";
